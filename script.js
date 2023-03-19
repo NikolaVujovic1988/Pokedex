d5ee0814d605f5cffbf0eb475a8e0691c8670fbf
Nikola Vujovic <nikola.vujovic1988@yahoo.com>
2023-03-19 21:23:21 +0100

    versuch vorschleife rendern

--- script.js
+++ script.js
@@ -3,20 +3,20 @@ let countPokemons = 0;
 let pokemons = [];
 
 async function loadPokemons() {
-    //for (let i = countPokemons; countPokemons < 20; i++) {
-    //    let url = `https://pokeapi.co/api/v2/pokemon/${i + 1}/`;
-    //    let response = await fetch(url);
-    //    currentPokemon = await response.json();
-    //    pokemons.push(currentPokemon);
-    //    console.log('loaded pokemon', currentPokemon);
-    //}
-    let url = 'https://pokeapi.co/api/v2/pokemon/1/';
-    let response = await fetch(url);
-    currentPokemon = await response.json();
+    for (let i = 1; i < 20; i++) {
+        let url = `https://pokeapi.co/api/v2/pokemon/${i + 1}/`;
+        let response = await fetch(url);
+        currentPokemon = await response.json();
+        pokemons.push(currentPokemon);
+        console.log('loaded pokemon', currentPokemon);
+    }
+    //let url = 'https://pokeapi.co/api/v2/pokemon/1/';
+    //let response = await fetch(url);
+    //currentPokemon = await response.json();
 
-    console.log('loaded pokemon', currentPokemon);
+    //console.log('loaded pokemon', currentPokemon);
     //countPokemons = i;
-    renderPokemonInfo();
+    //renderPokemonInfo();
 }
 
 function renderPokemonInfo() {
