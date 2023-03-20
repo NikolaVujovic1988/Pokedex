43fac8ea1484d1957caf7b9152b9adb4a5558b0f
Nikola Vujovic <nikola.vujovic1988@yahoo.com>
2023-03-20 12:18:40 +0100

    added types in btn

--- script.js
+++ script.js
@@ -1,37 +1,43 @@
 let currentPokemon;
-let countPokemons = 0;
-let pokemons = [];
 
 async function loadPokemons() {
     for (let i = 0; i < 20; i++) {
         let url = `https://pokeapi.co/api/v2/pokemon/${i + 1}/`;
         let response = await fetch(url);
         currentPokemon = await response.json();
-        pokemons.push(currentPokemon);
         console.log('loaded pokemon', currentPokemon);
-        document.getElementById('pokemonBigCard').innerHTML += smallCardTemplate();
+        document.getElementById('pokemonBigCard').innerHTML += smallCardTemplate(i);
+        loadTypes(currentPokemon, i);
     }
 }
 
-function smallCardTemplate() {
+function loadTypes(currentPokemon, i) {
+    for (let j = 0; j < currentPokemon["types"].length; j++) {
+        const type = currentPokemon["types"][j]["type"]["name"];
+        console.log(type);
+        document.getElementById(`pokemonsCardHeaderBtn${i}`).innerHTML += typeInfoTemplate(type);    
+    }
+}
+
+function typeInfoTemplate(type) {
+    return `
+    <button class="headerBtn">${type}</button>
+    `;
+}
+
+function smallCardTemplate(i) {
     return `
         <div class="pokemonsCard">
             <div class="pokemonsCardHeader">
                 <h2 class="pokemonName" id="pokemonName">${currentPokemon['name']}</h2>
                 <span id="pokemonNo">#${currentPokemon['id']}</span>
             </div>
-            <div class="pokemonsCardHeaderBtn" id="pokemonsCardHeaderBtn">
-                <button class="headerBtn">${currentPokemon['types']['0']['type']['name']}</button>
+            <div class="pokemonsCardHeaderBtn" id="pokemonsCardHeaderBtn${i}">
+                
             </div>
 
             <img id="pokemonImageLogo" src="./img/pokemon_logo_small.png">
             <img id="pokemonImage" src="${currentPokemon['sprites']['other']['dream_world']['front_default']}">
         </div>
     `;
-}
-
-async function renderPokemonInfo() {
-    document.getElementById('pokemonName').innerHTML = currentPokemon['name'];
-    document.getElementById('pokemonImage').src = currentPokemon['sprites']['other']['dream_world']['front_default'];
-    document.getElementById('pokemonNo'). innerHTML = `#${currentPokemon['id']}`;
 }
